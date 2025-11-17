0b066ed4cbc0196080e09afddd02366b1fea9929
1080heidi <heidideleon415@gmail.com>
2025-11-16 19:19:48 -0800

    added printing of inital state with 4 lanes

--- main.cpp
+++ main.cpp
@@ -22,53 +22,10 @@ int main() {
     cout << "Initial lanes:\n";
     for (int i = 0; i < LANES; i++) {
         cout << "Lane " << (i + 1) << ":\n";
-      
-
-    // add initial cars 
-    for (int i = 0; i < INITIAL; i++) {
-        line.push_back(Car()); // add to back of line
+        for (Car &c : lanes[i]) {
+            cout << "    ";
+            c.print();
+     }
     }
-
-    // queue display
-    cout << "Initial queue:\n";
-    for (Car &c : line) {
-        cout << "    ";
-        c.print();
-    }
-
-    int timeStep = 1;
-
-    // run simulation until line is empty
-    while (!line.empty()) {
-        
-        int r = rand() % 100; 
-
-        if (r < 55) {
-            cout << "\nTime: " << timeStep << " Operation: Car paid: ";
-            line.front().print();
-            line.pop_front();
-        }
-        else { // 45% chance a new car arrives
-            Car newCar;
-            cout << "\nTime: " << timeStep << " Operation: Joined lane: ";
-            newCar.print();
-            line.push_back(newCar);
-        }
-
-        // print queue after operation
-        cout << "Queue:\n";
-        if (line.empty()) {
-            cout << "    Empty\n";
-        }
-        else {
-            for (Car &c : line) {
-                cout << "    ";
-                c.print();
-            }
-        }
-
-        timeStep++;
-    }
-
     return 0;
 }
