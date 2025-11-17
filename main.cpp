4835474d2d0601081c5bb4dd9967e4553b41894d
1080heidi <heidideleon415@gmail.com>
2025-11-16 19:53:29 -0800

    fixing structure

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ int main() {
     srand(time(0)); // seed random number generator
     deque<Car> lanes[LANES]; // creates lanes, each is a deque<Car>
 
-    for (int i = 0; i < LANES; i++) {
+    for (int i = 0; i < LANES; i++) { // populate lanes
         for(int j = 0; j < 2; j++) {
             lanes[i].push_back(Car());
         }
     }
 
-    cout << "Initial lanes:\n";
+    cout << "Initial lanes:\n"; // print initial lanes
     for (int i = 0; i < LANES; i++) {
         cout << "Lane " << (i + 1) << ":\n";
         for (Car &c : lanes[i]) {
@@ -28,30 +28,42 @@ int main() {
      }
     }
 
-    for (int t = 1; t <= 20; t++) {
-        
+    for (int t = 1; t <= 20; t++) { // start time loop
+
         cout << "\nTime: " << t << "\n";
        
         for (int i = 0; i <= LANES; i++) { // operations for each lane
         
-            cout << "Time: " << t << "\n";
+            if (lanes[i].empty()) { // empty lane - 50/50
+                int r = rand() % 100;
 
-            int r = rand() % 100;
-
-            if (r < 50) {
-                cout << "Lane: " << i + 1 << "PaidL ";
-                lanes[i].front().print();
-                lanes[i].pop_front();
-            }
-            else {
+                  if (r < 50) {
                 Car newCar;
                 cout << "Lane: " << i + 1 << " Joined: ";
                 newCar.print();
-                lanes[i].push_back(newCar);
-                lanes[i].push_back(newCar);
+                lanes[i].push_back(newCar); 
+             } 
+            else {
+                cout << "Lane: " << i + 1 << " No action\n";
+            }
+      }
+            else { // non-empty lane - 50% paid, 50% joined
+                int r = rand() % 100;
+
+                if (r < 50) {
+                    cout << "Lane: " << i + 1 << " Paid: ";
+                    lanes[i].front().print();
+                    lanes[i].pop_front();
+                } else {
+                    Car newCar;
+                    cout << "Lane: " << i + 1 << " Joined: ";
+                    newCar.print();
+                    lanes[i].push_back(newCar);
+                }
+                
             }
         }
     }
-
-    return 0;
+    
+        // print queues for all lanes
 }
